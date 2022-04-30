d628c49a902485c710b2bd3076371a79d36f71b2
romaing <rocambille@gmail.com>
2022-04-30 09:04:44 +0200

    refactored Modal with subcomponents

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { node } from 'prop-types';
+
+const ModalContext = createContext();
 
 const useOpen = () => {
   const [isOpen, setOpen] = useState(false);
@@ -7,7 +9,7 @@ const useOpen = () => {
   return [isOpen, () => setOpen(true), () => setOpen(false)];
 };
 
-function Modal({ opener, children }) {
+function Modal({ children }) {
   const [isOpen, open, close] = useOpen();
 
   useEffect(() => {
@@ -27,31 +29,55 @@ function Modal({ opener, children }) {
   }, [close, isOpen]);
 
   return (
-    <>
-      {React.cloneElement(opener, { onClick: open })}
-      {isOpen && (
-        <div
-          className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-75 grid"
-          style={{
-            gridTemplateColumns: '1fr auto 1fr',
-            gridTemplateRows: '1fr auto 1fr',
-          }}>
-          <button
-            className="col-start-3 justify-self-start self-end"
-            type="button"
-            onClick={close}>
-            x
-          </button>
-          <div className="col-start-2 row-start-2">{children}</div>
-        </div>
-      )}
-    </>
+    <ModalContext.Provider value={{ isOpen, open, close }}>
+      {children}
+    </ModalContext.Provider>
   );
 }
 
 Modal.propTypes = {
-  opener: node.isRequired,
   children: node.isRequired,
 };
+
+function Trigger({ children }) {
+  const { open } = useContext(ModalContext);
+
+  return React.cloneElement(children, { onClick: open });
+}
+
+Trigger.propTypes = {
+  children: node.isRequired,
+};
+
+Modal.Trigger = Trigger;
+
+function Content({ children }) {
+  const { isOpen, close } = useContext(ModalContext);
+
+  return (
+    isOpen && (
+      <div
+        className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-75 grid"
+        style={{
+          gridTemplateColumns: '1fr auto 1fr',
+          gridTemplateRows: '1fr auto 1fr',
+        }}>
+        <button
+          className="col-start-3 justify-self-start self-end"
+          type="button"
+          onClick={close}>
+          x
+        </button>
+        <div className="col-start-2 row-start-2">{children}</div>
+      </div>
+    )
+  );
+}
+
+Content.propTypes = {
+  children: node.isRequired,
+};
+
+Modal.Content = Content;
 
 export default Modal;
